6d05920f503771dec4fe1f622ca2a010ca506d73
kev01998 <kevinjayeshkumar797@gmail.com>
2022-11-21 15:36:48 +0100

    adding new test case

--- images/example-python/HandsOn.py
+++ images/example-python/HandsOn.py
@@ -55,16 +55,26 @@ mydb = pymysql.connect(host='localhost',
 #cursor.close()
    
  #output the table to a csv file
-cursor = mydb.cursor()
-sql = "select places.country, count(people.given_name) from places inner join people on places.city = people.place_of_birth group by country"
+#cursor = mydb.cursor()
+#sql = "select places.country, count(people.given_name) from places inner join people on places.city = people.place_of_birth group by country"
 
 
-cursor.execute(sql)
-rows = cursor.fetchall()
+#cursor.execute(sql)
+#rows = cursor.fetchall()
 #for row in rows:
   #print(row[0])
-with open(r'C:\Users\KevinAiwala\Downloads\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\DE challenge\data\handsonc_output.csv', 'w') as csv_file:
-  writer = csv.writer(csv_file)
-  writer.writerows(rows)
+#with open(r'C:\Users\KevinAiwala\Downloads\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\DE challenge\data\handsonc_output.csv', 'w') as csv_file:
+  #writer = csv.writer(csv_file)
+  #writer.writerows(rows)
+#mydb.commit()
+#cursor.close()
+
+#output the csv file to json file
+cursor = mydb.cursor()
+rows = csv.reader(open(r'C:\Users\KevinAiwala\Downloads\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\DE challenge\data\people.csv',encoding="utf8"))
+with open(r'C:\Users\KevinAiwala\Downloads\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\Solution_DE-Challenge_Kevin-main\DE challenge\data\cToJ_output .json', 'w') as json_file:
+  rows = [{'given_name':row[0], 'family_name':row[1], 'date_of_birth' : row[2], 'place_of_birth':row[1]} for row in rows]
+  json.dump(rows,json_file)
 mydb.commit()
-cursor.close()
+cursor.close()  
+  
